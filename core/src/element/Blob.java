caeb9cd1cd5aecb7d9f5e8aaf7044b2aea077833
shaba81 <alfano.angelo97@gmail.com>
2017-12-13 16:03:07 +0100

    Added other blobs (to be fixed)

--- core/src/element/Blob.java
+++ core/src/element/Blob.java
@@ -1,24 +1,32 @@
 package element;
 
-import com.badlogic.gdx.math.Vector2;
-
 public class Blob {
 	
-	private Vector2 position;
+	private int x;
+	private int y;
 	private int radius;
 	
-	public Blob(Vector2 position, int radius) {
-		
-		this.position = position;
+	
+	public Blob(int x, int y, int radius) {
+		this.x = x;
+		this.y = y;
 		this.radius = radius;
 	}
 
-	public Vector2 getPosition() {
-		return position;
+	public int getX() {
+		return x;
 	}
 
-	public void setPosition(Vector2 position) {
-		this.position = position;
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
 	}
 
 	public int getRadius() {
--- core/src/graphics/Render.java
+++ core/src/graphics/Render.java
@@ -1,5 +1,8 @@
 package graphics;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -14,14 +17,27 @@ import element.Blob;
 public class Render extends ApplicationAdapter {
 	SpriteBatch batch;
 	ShapeRenderer shapeRenderer;
+	ArrayList<Blob> blobs;
 	Blob blob;
+	
+	private int numBlobs = 10;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
 		
-		blob = new Blob(new Vector2(600/2,400/2), 60);
+		blob = new Blob(600/2,400/2, 60);
+		initBlobs();
+	}
+	
+	public void initBlobs() {
+		blobs = new ArrayList<Blob>(numBlobs);
+		for (int i = 0; i < numBlobs; i++) {
+			Blob b = new Blob((int) Math.random() * 500,(int) Math.random() * 300,(int) Math.random() * 30);
+			blobs.add(b);
+		}
+		System.out.println(blobs.size());
 	}
 
 	@Override
@@ -31,7 +47,11 @@ public class Render extends ApplicationAdapter {
 		batch.begin();
 		shapeRenderer.begin(ShapeType.Filled);
 		
-		shapeRenderer.circle(blob.getPosition().x, blob.getPosition().y, blob.getRadius());
+		shapeRenderer.circle(blob.getX(), blob.getY(), blob.getRadius());
+		
+		for (Blob b : blobs) {
+			shapeRenderer.circle(b.getX(), b.getY(), b.getRadius());
+		}
 
 		shapeRenderer.end();
 		batch.end();
